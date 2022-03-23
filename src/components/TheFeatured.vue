d91962874818fbe60122dd1fb1aca8bd608640a1
Christian Gil <christian@chrisgvdev.com>
2022-03-23 16:34:58 -0600

    update: Change images of the featured section

--- src/components/TheFeatured.vue
+++ src/components/TheFeatured.vue
@@ -2,8 +2,8 @@
 import { gsap } from 'gsap';
 
 const [firstImg, secondImg] = [
-  'https://picsum.photos/300/400?random=1',
-  'https://picsum.photos/400/600?random=1',
+  'https://picsum.photos/id/1005/300/400',
+  'https://picsum.photos/id/1015/800/800',
 ];
 
 const image1 = ref<HTMLImageElement>();
@@ -26,18 +26,18 @@ onMounted(() => {
 
 <template>
   <section id="featured-section" class="grid grid-cols-[1fr,3fr] items-center px-[5vw] py-24">
-    <div id="featured-row-layout">
+    <div>
       <h6 class="font-work text-xl font-light uppercase">Decoración</h6>
       <img ref="image1" :src="firstImg" class="w-full object-cover" />
     </div>
 
-    <div id="featured-column-layout" class="flex items-end justify-start">
+    <div class="flex items-end justify-start">
       <h6
         class="origin-bottom-left translate-x-full -rotate-90 transform font-work text-xl font-light uppercase"
       >
         Presentación
       </h6>
-      <img ref="image2" :src="secondImg" class="h-[125vh] w-full object-cover" />
+      <img ref="image2" :src="secondImg" class="h-[100vh] w-full object-cover" />
     </div>
   </section>
 </template>
