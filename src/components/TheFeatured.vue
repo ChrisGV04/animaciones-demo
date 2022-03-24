47fec532f97ddeced520f9f789eedc17c4b09abb
Christian Gil <christian@chrisgvdev.com>
2022-03-23 22:56:43 -0600

    update: Make the featured section responsive

--- src/components/TheFeatured.vue
+++ src/components/TheFeatured.vue
@@ -5,23 +5,31 @@ const image1 = ref<HTMLImageElement>();
 const image2 = ref<HTMLImageElement>();
 
 onMounted(() => {
-  gsap.to(image1.value!, {
-    duration: 0.75,
-    scrollTrigger: image1.value,
-    clipPath: 'inset(0% 0% 0% 0%)',
-  });
+  gsap.fromTo(
+    image1.value!,
+    { clipPath: 'inset(100% 0% 0% 0%)' },
+    {
+      duration: 0.75,
+      scrollTrigger: image1.value,
+      clipPath: 'inset(0% 0% 0% 0%)',
+    }
+  );
 
-  gsap.to(image2.value!, {
-    duration: 1.5,
-    scrollTrigger: image2.value,
-    clipPath: 'inset(0% 0% 0% 0%)',
-  });
+  gsap.fromTo(
+    image2.value!,
+    { clipPath: 'inset(100% 0% 0% 0%)' },
+    {
+      duration: 1.5,
+      scrollTrigger: image2.value,
+      clipPath: 'inset(0% 0% 0% 0%)',
+    }
+  );
 });
 </script>
 
 <template>
-  <section id="featured-section" class="grid grid-cols-[1fr,3fr] items-center px-[5vw] py-24">
-    <div>
+  <section id="featured-section" class="flex flex-col items-center gap-4 px-[5vw] py-12 md:flex-row md:py-24">
+    <div class="md:w-2/5 lg:w-1/3">
       <h6 class="font-work text-xl font-light uppercase">Decoración</h6>
       <img
         ref="image1"
@@ -30,15 +38,11 @@ onMounted(() => {
       />
     </div>
 
-    <div class="flex items-end justify-start">
-      <h6
-        class="origin-bottom-left translate-x-full -rotate-90 transform font-work text-xl font-light uppercase"
-      >
-        Presentación
-      </h6>
+    <div class="flex w-full flex-col items-end justify-start md:flex-row md:pl-10 lg:pl-20">
+      <h6 class="custom-text font-work text-xl font-light uppercase">Presentación</h6>
       <img
         ref="image2"
-        class="h-[100vh] w-full object-cover"
+        class="w-full flex-1 object-cover md:h-[80vh] lg:h-screen"
         src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=40"
       />
     </div>
@@ -46,7 +50,10 @@ onMounted(() => {
 </template>
 
 <style scoped>
-img {
-  clip-path: inset(100% 0% 0% 0%);
+@media (min-width: 768px) {
+  .custom-text {
+    writing-mode: vertical-lr;
+    transform: rotate(180deg);
+  }
 }
 </style>
