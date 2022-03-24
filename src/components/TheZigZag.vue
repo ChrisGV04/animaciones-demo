944b7b3e4be9b64d4160bd4dd9c5c4c47125bee2
Christian Gil <christian@chrisgvdev.com>
2022-03-24 00:33:23 -0600

    update: Improve responsiveness of the zig zag section

--- src/components/TheZigZag.vue
+++ src/components/TheZigZag.vue
@@ -36,15 +36,19 @@ const setAnimations = () => {
 <template>
   <section ref="demoWrapper" class="space-y-4 overflow-x-hidden py-24">
     <div :ref="(el:any) => galleries.push(el)" class="text">
-      <div class="gallery-wrapper w-max">PRESENTACION MODERNA</div>
+      <div class="gallery-wrapper w-max">PRESENTACIÓN MODERNA</div>
     </div>
 
     <div :ref="(el:any) => galleries.push(el)" v-for="gIdx in 3" :key="`g${gIdx}`">
       <ul class="gallery-wrapper flex gap-4">
-        <li v-for="imIdx in 3" :key="`g${gIdx}i${imIdx}`" class="w-[clamp(500px,60vw,800px)] shrink-0">
+        <li
+          v-for="imIdx in 3"
+          :key="`g${gIdx}i${imIdx}`"
+          class="w-screen shrink-0 md:w-[clamp(500px,60vw,800px)]"
+        >
           <img
             :ref="(el:any) => images.push(el)"
-            class="h-[40vh] w-full object-cover"
+            class="h-[30vh] w-full object-cover md:h-[40vh]"
             :src="sources[(gIdx - 1) * gIdx + imIdx - 1]"
           />
         </li>
@@ -52,13 +56,13 @@ const setAnimations = () => {
     </div>
 
     <div :ref="(el:any) => galleries.push(el)" class="text">
-      <div class="gallery-wrapper w-max">PRESENTACION MODERNA</div>
+      <div class="gallery-wrapper w-max">EFECTOS IMPACTANTES</div>
     </div>
   </section>
 </template>
 
 <style scoped>
 .text {
-  @apply font-bai text-[clamp(8rem,15vw,16rem)] font-bold leading-none;
+  @apply font-bai text-[clamp(4rem,15vw,10rem)] font-bold leading-none;
 }
 </style>
